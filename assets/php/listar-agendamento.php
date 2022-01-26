d7bd3ee98b13823edce67dafaf57e100f9c9b328
Douglas Silva <silvatec.br@gmail.com>
2022-01-26 19:54:34 -0300

    update 3.0

--- assets/php/listar-agendamento.php
+++ assets/php/listar-agendamento.php
@@ -4,16 +4,17 @@
     <head>
         <!-- Metadados -->
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <!-- Título da página -->
-        <title>Agendamentos</title>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <!-- Título da página -->
+        <title>Agendamentos</title>
     </head>
 
     <body class="p-3 mb-2 bg-light text-dark">
