5a0a8723ed419c75c1ad6e63a55ab0bcaabf2d85
Douglas Silva <silvatec.br@gmail.com>
2022-01-27 10:05:02 -0300

    update 3.1

--- assets/php/listar-agendamento.php
+++ assets/php/listar-agendamento.php
@@ -13,6 +13,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
+        <!-- Icone da página -->
+        <link rel = "shortcut icon" type = "imagem/x-icon" href = "./favicon.ico"/>
+
         <!-- Título da página -->
         <title>Agendamentos</title>
     </head>
