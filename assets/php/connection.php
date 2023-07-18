f5696f39b21237c7f5d4487ff5d9232281f47955
Doug-Silva <silvatec.br@gmail.com>
2023-07-18 07:45:19 -0300

    update 4.3

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,6 +1,3 @@
 <?php
-  $mysqli = mysqli_init();
-  //$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-  $mysqli->close();
+  $conn = mysqli_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
 ?>
